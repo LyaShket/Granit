38b33707c1e1a83114ec3de0edc09804d4d4a9d9
LyaShket <dingofleep@gmail.com>
2020-04-06 20:44:00 +0300

    lazy load the google map

--- js/script.js
+++ js/script.js
@@ -51,3 +51,13 @@ $(document).ready(function(){
 		$('body,html').animate({scrollTop: top - 80}, 1500);
 	})
 });
+
+// lazy load the google map
+let popular_top = $(".popular").offset().top;
+$(window).bind("scroll", function(){
+	let window_top = $(this).scrollTop();
+	if (window_top > popular_top) {
+		$('.contacts__google-map').attr("src", "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4485.602906394397!2d37.97032521489928!3d55.796685453930074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x414acc159d4b0b31%3A0x5ce4eca0b592f57f!2sKFC!5e0!3m2!1sru!2sua!4v1584976715538!5m2!1sru!2sua");
+		$(window).unbind("scroll");
+	}
+});
